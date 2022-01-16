a4408751584486e969fa01126b9456d21c5190cb
Eugene Dudnyk <edudnyk@gmail.com>
2022-01-16 11:12:28 +0100

    Updated Copyright term.

--- Sources/SheeKit/SheetPresenterController.swift
+++ Sources/SheeKit/SheetPresenterController.swift
@@ -6,7 +6,7 @@
 //
 //  MIT License
 //
-//  Copyright (c) 2021 Eugene Dudnyk
+//  Copyright (c) 2021-2022 Eugene Dudnyk
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
